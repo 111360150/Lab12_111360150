e910678775d06c89578614aa8c70bd572501c1a6
111360150 <t111360150@ntut.org.tw>
2024-12-20 15:56:40 +0800

    your commit content.

---
+++ app/src/main/java/com/example/lab12/UnsafeOkHttpClient.java
@@ -0,0 +1,46 @@
+package com.example.lab12;
+
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+import okhttp3.OkHttpClient;
+
+public class UnsafeOkHttpClient {
+    public static OkHttpClient getUnsafeOKHttpClient() {
+        try {
+            final TrustManager[] trustAllCerts = new TrustManager[]{
+                    new X509TrustManager() {
+                        @Override
+                        public void checkClientTrusted(X509Certificate[] x509Certificates, String s) {
+                        }
+
+                        @Override
+                        public void checkServerTrusted(X509Certificate[] x509Certificates, String s) {
+                        }
+
+                        @Override
+                        public X509Certificate[] getAcceptedIssuers() {
+                            return new X509Certificate[]{};
+                        }
+                    }
+            };
+
+            // 創建 SSLContext 實例
+            final SSLContext sslContext = SSLContext.getInstance("SSL");
+
+            // 呼叫實例方法 init
+            sslContext.init(null, trustAllCerts, new SecureRandom());
+
+            return new OkHttpClient
+                    .Builder()
+                    .sslSocketFactory(sslContext.getSocketFactory(), (X509TrustManager) trustAllCerts[0])
+                    .hostnameVerifier((hostname, session) -> true)
+                    .build();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
